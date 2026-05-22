20a26895a080f71ce71d57bd9df437a91959224f
Jaid <6216144+Jaid@users.noreply.github.com>
2026-05-22 23:52:53 +0100

    Improved agnostic composer
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/lib/composer/AgnosticIdComposer.ts
+++ src/lib/composer/AgnosticIdComposer.ts
@@ -4,7 +4,7 @@ import {hashStringToUint16} from '#src/lib/hash.ts'
 
 export default class AgnosticIdComposer extends IdComposer {
   constructor(machineIdOverride?: string, sessionIdOverride?: string) {
-    const machineId = machineIdOverride ? hashStringToUint16(machineIdOverride) : 0
+    const machineId = hashStringToUint16(machineIdOverride ?? Object.keys(globalThis).join('\0'))
     const sessionId = sessionIdOverride ? hashStringToUint16(sessionIdOverride) : getRandomUint16()
     super(machineId, sessionId)
   }
